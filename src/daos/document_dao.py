8260dbedb90df818ba6094fa21249993ad0bc42b
charlieclarke <charlie.2.clarke@bt.com>
2021-04-10 23:43:27 +0100

    initial counting of content

---
+++ src/daos/document_dao.py
@@ -0,0 +1,35 @@
+import docx
+from models.document import Document
+
+
+class DocumentDao:
+    def __init__(self, file_path):
+        self.document = Document(file_path)
+
+        self.initialise_document()
+
+    def initialise_document(self):
+        self.document.paras = list(self.set_paragraphs())
+        self.document.words = list(self.set_words())
+        self.print_word_count()
+
+    def set_paragraphs(self):
+        counter = 0
+
+        while self.document.doc.paragraphs[counter].text != 'Appendices':
+            paragraph = self.document.doc.paragraphs[counter]
+            counter += 1
+
+            if not paragraph.style.name.startswith('Heading') and paragraph.text != '':
+                yield paragraph
+
+    def set_words(self):
+        for paragraph in self.document.paras:
+            paragraph = paragraph.text
+            words = paragraph.split()
+            for word in words:
+                if word != '.':
+                    yield
+
+    def print_word_count(self):
+        print(len(self.document.words))
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from daos.document_dao import DocumentDao
+
+
+if __name__ == '__main__':
+    file_path = input('File path to document: ')
+    print('')
+    document_dao = DocumentDao(file_path)
