b14b84a0df92bec3ab0db12afd3f517e4e7e7fd6
FreddyC08 <freddyclarke51@gmail.com>
2023-05-01 17:22:49 +0100

    + Documentation, + Error handling, - Index error

--- src/daos/document_dao.py
+++ src/daos/document_dao.py
@@ -3,26 +3,32 @@ from models.document import Document
 
 
 class DocumentDao:
+    ''' Data access object for interacting with word documents '''
+
     def __init__(self, file_path):
+        ''' Initialises the document instance with the file path '''
         self.document = Document(file_path)
 
         self.initialise_document()
 
     def initialise_document(self):
+        ''' Initialises the document instance with the tables, paragraphs and words '''
         self.document.tables = self.document.doc.tables
         self.document.paras = list(self.set_paragraphs())
         self.document.words = list(self.set_words())
 
     def set_paragraphs(self):
+        ''' Yields all the paragraphs in the document, skipping headings, 
+            empty paragraphs, figure captions, and the appendices '''
         counter = 0
 
-        while self.document.doc.paragraphs[counter].text != 'Appendices':
+        while counter < len(self.document.doc.paragraphs) and self.document.doc.paragraphs[counter].text != 'Appendices':
             paragraph = self.document.doc.paragraphs[counter]
             counter += 1
 
-            if not paragraph.style.name.startswith('Heading') and \
-                paragraph.text != '' and not paragraph.text.startswith('Figure'):
-                yield paragraph
+        if not paragraph.style.name.startswith('Heading') and \
+            paragraph.text != '' and not paragraph.text.startswith('Figure'):
+            yield paragraph 
 
         for table in self.document.tables:
             for row in table.rows:
@@ -32,12 +38,15 @@ class DocumentDao:
                             yield paragraph
 
     def set_words(self):
+        ''' Yields words from the paragraphs, excluding full stops and dashes '''
         for paragraph in self.document.paras:
             paragraph = paragraph.text
             words = paragraph.split()
             for word in words:
+                print(word)
                 if word != ('.' or '–'):
                     yield word
 
     def print_word_count(self):
+        ''' Prints total word count of document '''
         print(len(self.document.words))
--- src/models/document.py
+++ src/models/document.py
@@ -1,11 +1,15 @@
 import docx
 
 class Document:
+    ''' Represents a document with tables, paragraphs, and words '''
+
     def __init__(self, file_path):
+        ''' Initialises the document instance with the file path '''
         try:
             self.doc = docx.Document(file_path)
-        except Exception:
-            print("Document couldn't be found...")
+        except Exception as e:
+            raise FileNotFoundError(f"Document couldn't be found at {file_path}") from e
+            #print(f"Document couldn't be found at {file_path}")
 
         self.tables = []
         self.paras = []
