68ab9e99bdd9e80581ada0aef967b6320644865f
FreddyC08 <freddyclarke51@gmail.com>
2023-05-02 21:20:41 +0100

    feat: add command-line argument for file path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import sys
 from daos.document_dao import DocumentDao
 
 
 if __name__ == '__main__':
-    file_path = input('File path to document: ')
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        file_path = input('Enter file path to document without quotation marks: ')
     print('')
     document_dao = DocumentDao(file_path)
     if document_dao.document_loaded():
