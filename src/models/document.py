8269843ab8867a001f8a48e71d167ff4757c6fd3
FreddyC08 <freddyclarke51@gmail.com>
2023-05-02 21:38:58 +0100

    fix: removed unnecessary string formatting

--- src/models/document.py
+++ src/models/document.py
@@ -11,11 +11,11 @@ class Document:
         try:
             self.doc = docx.Document(file_path)
         except FileNotFoundError:
-            print(f"File not found. Please ensure the file path is correct and not contained within speech or quotation marks.")
+            print("File not found. Please ensure the file path is correct and not contained within speech or quotation marks.")
         except PermissionError:
-            print(f"Permission denied. Please ensure the file is not open in another program.")
+            print("Permission denied. Please ensure the file is not open in another program.")
         except PackageNotFoundError:
-            print(f"Package not found. Please ensure the file is closed in other programs and has a valid '.docx' format.")
+            print("Package not found. Please ensure the file is closed in other programs and has a valid '.docx' format.")
         except Exception as e:
             print(f"An unexpected error occurred: {str(e)}.\nPlease ensure the following:\n\t1. The file path is correct.\n\t2. The file path is not contained within speech or quotation marks.\n\t3. The file is not open in another program.")
             return
